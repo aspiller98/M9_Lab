19ab944932203b0c5b00bb6b7f7efe4b07162911
aspiller98 <43651778+aspiller98@users.noreply.github.com>
2020-10-19 14:30:26 -0400

    Lab

---
+++ urlextract.py
@@ -0,0 +1,16 @@
+
+    
+from bs4 import BeautifulSoup
+
+import requests
+
+url = raw_input("Please enter the URL you wish to extract data from: ")
+
+r  = requests.get("http://" +url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
